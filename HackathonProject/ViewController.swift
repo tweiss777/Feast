ffeae6315f6af432bfd01ddaced0c02825901b36
tweiss777 <tweiss747@gmail.com>
2016-11-10 20:19:07 -0500

    Initial Commit

---
+++ HackathonProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackathonProject
+//
+//  Created by Tal Weiss on 11/10/16.
+//  Copyright © 2016 Tal Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
